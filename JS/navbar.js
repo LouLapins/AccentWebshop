a0cd40eece4c87368981e8c150e097e5178b2559
LouLapins <loulapins@hotmail.com>
2021-03-11 16:55:22 +0100

    fix link bug

--- JS/navbar.js
+++ JS/navbar.js
@@ -4,27 +4,27 @@ $(function() {
 });
 
 function renderNavbar() {
-    $( ".hamburgerBtn" )
-    .on("click", function() {
-        $( ".myUl" ).slideToggle();
-        $( ".myUl" ).css({
-            display: "flex"
-        })
-        $("main").fadeToggle()
-    });
+    $(".hamburgerBtn")
+        .on("click", function() {
+            $(".myUl").slideToggle();
+            $(".myUl").css({
+                display: "flex"
+            })
+            $("main").fadeToggle()
+        });
 
     //HYPERLINK SHOPPINGCARTBUTTON -> CART
-    $( ".shoppingCartBtn",)
-    .on ("click", function(){
-    window.location.href = "../HTML/cart.html";
-    });
+    $(".shoppingCartBtn", )
+        .on("click", function() {
+            window.location.href = "HTML/cart.html";
+        });
 
     //HYPERLINK LOGOTYPE -> HOME
-    $( ".logotype" )
-    .on("click", function(){
-    window.location.href = "../index.html";
-    console.log("klick");
-    });
+    $(".logotype")
+        .on("click", function() {
+            window.location.href = "index.html";
+            console.log("klick");
+        });
 }
 
 function cartNumbers() {
@@ -32,11 +32,11 @@ function cartNumbers() {
     getFromLocalStorage();
 
     let itemsInCart = 0;
-    
+
     for (let i = 0; i < cart.length; i++) {
         itemsInCart += cart[i].quantity;
-    }  
+    }
 
     $("#counterContainer")
-    .html(itemsInCart);
+        .html(itemsInCart);
 }
